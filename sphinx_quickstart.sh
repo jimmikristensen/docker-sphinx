0139cce15dd7bff9b9efa373715966d2ca035b3b
Jimmi Kristensen <jikr@tv2.dk>
2017-12-29 14:12:48 +0100

    script for sphinx-quickstart

--- sphinx_quickstart.sh
+++ sphinx_quickstart.sh
@@ -2,11 +2,11 @@
 
 sphinx-quickstart  -p 'myproject' -a 'me' --sep --dot=. -v 1.0 --suffix=.rst --master=index --extensions=sphinxcontrib.plantuml -q /doc
 
-sed -i "s/# import os/import os/g" /doc/conf.py
-sed -i "s/project = .*/project = os.environ[\'projectName\']/g" /doc/conf.py
-sed -i "s/copyright = .*/copyright = os.environ[\'owner\']/g" /doc/conf.py
-sed -i "s/author = .*/author = os.environ[\'owner\']/g" /doc/conf.py
-sed -i "s/version = .*/version = os.environ[\'major\']\+\".\"\+os.environ[\'minor\']/g" /doc/conf.py
-sed -i "s/release = .*/release = os.environ[\'major\']\+\".\"\+os.environ[\'minor\']/g" /doc/conf.py
-sed -i "s/html_theme = \'.*\'/html_theme = \'sphinx_rtd_theme\'/g" /doc/conf.py
+sed -i "s/# import os/import os/g" source/conf.py
+sed -i "s/project = .*/project = os.environ[\'projectName\']/g" source/conf.py
+sed -i "s/copyright = .*/copyright = os.environ[\'owner\']/g" source/conf.py
+sed -i "s/author = .*/author = os.environ[\'owner\']/g" source/conf.py
+sed -i "s/version = .*/version = os.environ[\'major\']\+\".\"\+os.environ[\'minor\']/g" source/conf.py
+sed -i "s/release = .*/release = os.environ[\'major\']\+\".\"\+os.environ[\'minor\']/g" source/conf.py
+sed -i "s/html_theme = \'.*\'/html_theme = \'sphinx_rtd_theme\'/g" source/conf.py
 
