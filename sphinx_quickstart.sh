84fe5e8e595aa67bc06e4efbe87739733ab88279
Jimmi Kristensen <jikr@tv2.dk>
2017-12-31 16:06:01 +0100

    added project name as env var

--- sphinx_quickstart.sh
+++ sphinx_quickstart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sphinx-quickstart  -p 'myproject' -a 'me' --sep --dot=. -v 1.0 --suffix=.rst --master=index --extensions=sphinxcontrib.plantuml -q /doc
+sphinx-quickstart  -p $projectName -a 'me' --sep --dot=. -v 1.0 --suffix=.rst --master=index --extensions=sphinxcontrib.plantuml -q /doc
 
 sed -i "s/# import os/import os/g" source/conf.py
 sed -i "s/project = .*/project = os.environ[\'projectName\']/g" source/conf.py
@@ -9,4 +9,5 @@ sed -i "s/author = .*/author = os.environ[\'owner\']/g" source/conf.py
 sed -i "s/version = .*/version = os.environ[\'major\']\+\".\"\+os.environ[\'minor\']/g" source/conf.py
 sed -i "s/release = .*/release = os.environ[\'major\']\+\".\"\+os.environ[\'minor\']/g" source/conf.py
 sed -i "s/html_theme = \'.*\'/html_theme = \'sphinx_rtd_theme\'/g" source/conf.py
+
 
