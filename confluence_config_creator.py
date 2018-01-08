04b21b9cc73961499c38b9f0159aee8987826e93
Jimmi Kristensen <jikr@tv2.dk>
2018-01-08 15:14:54 +0100

    creating confluence config creator

--- confluence_config_creator.py
+++ confluence_config_creator.py
@@ -1,42 +1,95 @@
 import os
 import sys
 import glob
+'''
+confluencePages='README:160465230,index:235012393'
+confluenceUrl='https://tv2cms.atlassian.net/wiki'
+baseDir='/doc/sphinxdoc/build/json'
+configDir='/doc/sphinxdoc'
+downloadDir='_downloads'
+imgDir='_images'
+srcExt='.fjson'
+'''
 
-confluenceAuthPages='README:160465230,index:235012393'
 
-def createPageString():
+
+def createPageString(confluencePages, imagesPath, downloadsPath):
     pagesStr=''
-    pages=confluenceAuthPages.split(',')
+    pages=confluencePages.split(',')
 
     for page in pages:
         pageParts = page.split(':');
         pageFile = pageParts[0]
         pageConfluenceId = pageParts[1]
+        attachmentsStr = createAttachmentsString(imagesPath, downloadsPath)
 
         pageStr = "- id: %s\n  source: %s" % (pageConfluenceId, pageFile)
         if pagesStr != '':
-            pagesStr = "%s\n%s" % (pagesStr, pageStr)
+            pagesStr = "%s\n%s%s" % (pagesStr, pageStr, attachmentsStr)
         else:
-            pagesStr = pageStr
+            pagesStr = "%s%s" % (pageStr, attachmentsStr)
 
+    if pageStr == '':
+        raise ValueError('Unable to parse confluencePages - is empty or malformed')
+
+    pagesStr = "pages:\n%s" % pagesStr
     return pagesStr
+
+def createAttachmentsString(imagesPath, downloadsPath):
+    attachmentsStr = ''
+    imagesStr = createImagesString('/home/jimmi/IdeaProjects/play-template/doc/sphinxdoc/build/json/_images')
+    downloadStr = createDownloadsString('/home/jimmi/IdeaProjects/play-template/doc/sphinxdoc/build/json/_downloads')
+    
+    if downloadStr != '' or imagesStr != '':
+        attachmentsStr = "%s\n  attachments:" % attachmentsStr
+        if imagesStr != '':
+            attachmentsStr = "%s\n%s" % (attachmentsStr, imagesStr)
+        if downloadStr != '':
+            attachmentsStr = "%s\n%s" % (attachmentsStr, downloadStr)
+    return attachmentsStr
 
 def createImagesString(imageDir):
     imagesStr = ''
     imageFiles = glob.glob(imageDir+'/*')
         
     if (len(imageFiles) > 0):
-        imagesStr = "  attachments:"
-        
-    for imageFile in imageFiles:
-        imagesStr = "%s\n  - %s" % (imagesStr, imageFile)
-
+        imagesStr = '    images:'
+        for imageFile in imageFiles:
+            imagesStr = "%s\n    - %s" % (imagesStr, imageFile)
     return imagesStr
 
-print createPageString()
-print createImagesString('/Users/jikr/IdeaProjects/play-template-app/doc/sphinxdoc/build/json/_images')
+def createDownloadsString(downloadDir):
+    downloadStr = ''
+    downloadFiles = glob.glob(downloadDir+'/*')
+
+    if (len(downloadFiles) > 0):
+        downloadStr = '    downloads:'
+        for downloadFile in downloadFiles:
+            downloadStr = "%s\n    - %s" % (downloadStr, downloadFile)
+    return downloadStr
+            
+
+def createConfigYaml(confluencePages, confluenceUrl, baseDir, downloadDir, imgDir, srcExt):
+    imagesPath = "%s/%s" % (baseDir, imgDir)
+    downloadsPath = "%s/%s" % (baseDir, downloadDir)
+    pageStr = createPageString(confluencePages)
+    
+    yamlStr = (
+        "version: 2\n"
+        "url: %s\n"
+        "base_dir: %s\n"
+        "downloads_dir: %s\n"
+        "images_dir: %s\n"
+        "source_ext: %s\n"
+        "%s"
+        % (confluenceUrl, baseDir, downloadDir, imgDir, srcExt, pageStr)
+    )
+
+    return yamlStr
+
+print createConfigYaml(confluencePages, confluenceUrl, baseDir, downloadDir, imgDir, srcExt)
 if 'confluenceAuth' not in os.environ:
     sys.exit('Environment variable confluenceAuth is missing')
 
-if 'confluenceAuthPages' not in os.environ:
+if 'confluencePages' not in os.environ:
     sys.exit('Environment variable confluencePages is missing')
