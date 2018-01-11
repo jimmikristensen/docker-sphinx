a6eb5dac9d2fb835b7c71ce5201e82e626a964cf
Jimmi Kristensen <jikr@tv2.dk>
2018-01-11 19:52:45 +0100

    added replace in file function

--- confluence_publish_sphinx_doc.py
+++ confluence_publish_sphinx_doc.py
@@ -3,6 +3,8 @@ import os
 import sys
 import glob
 import re
+import shutil
+import tempfile
 from conf_publisher import publish
 
 def createPageString(confluencePages, imagesPath, downloadsPath, configFilePath):
@@ -128,6 +130,23 @@ def writeConfigYaml(configFilePath, configStr):
     fh.write(configStr)
     fh.close()
 
+def replace_in_file(filename, pattern, replace):
+    '''
+    Perform a Python equivalent of in-place `sed` substitution: e.g.,
+    `sed -i -e 's/'${pattern}'/'${repl}' "${filename}"`.
+    '''
+    # For efficiency, precompile the passed regular expression.
+    pattern_compiled = re.compile(pattern)
+
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp_file:
+        with open(filename) as src_file:
+            for line in src_file:
+                tmp_file.write(pattern_compiled.sub(replace, line))
+
+    #overwrite original file with temp file such that file attributes are preserved
+    shutil.copystat(filename, tmp_file.name)
+    shutil.move(tmp_file.name, filename)
+
 def main():
     if 'confluenceAuth' not in os.environ:
         sys.exit('Environment variable confluenceAuth is missing')
@@ -162,6 +181,12 @@ def main():
     configYamlStr = createConfigYaml(confluencePages, confluenceUrl, basePath, downloadDir, imgDir, srcExt, configFilePath)
     writeConfigYaml(configFilePath, configYamlStr)
 
+    '''
+    filename=sys.argv[1]
+    img=sys.argv[2]
+    replace_in_file(filename, r'<p class=\\"plantuml\\">\\n<img src=\\"\.\./_images/%s.*?</p>' % img, '<p><ac:image><ri:attachment ri:filename=\\"%s\\" ri:version-at-save=\\"1\\" /></ac:image></p>' % img)
+    '''
+    
     sys.argv = [sys.argv[0], '-F', configFilePath, '--auth', confluenceAuth]
     print ''
     print 'Executing conf_publisher.publish with the following arguments:'
